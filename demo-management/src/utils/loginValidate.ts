fdc9b319f2c9e13bfca9bf93610adb081cace0f3
wbyanyue <wbyanyue@centaline.com.cn>
2021-01-04 16:41:33 +0800

    表单校验和类型匹配

---
+++ demo-management/src/utils/loginValidate.ts
@@ -0,0 +1,42 @@
+import {ref,getCurrentInstance} from 'vue'
+interface User{
+    email: string;
+    password: string;
+}
+interface Rules{
+    email: {
+        type: string;
+        message: string;
+        required: boolean;
+        trigger: string;
+    }[];
+    password: ({
+        required: boolean;
+        message: string;
+        trigger: string;
+        min?: undefined;
+        max?: undefined;
+    } | {
+        min: number;
+        max: number;
+        message: string;
+        trigger: string;
+        required?: undefined;
+    })[];
+}
+export const loginUser=ref<User>({
+    email:'',
+    password:''
+})
+// 校验规则
+export const rules=ref({
+  email: [
+    { type: 'email',message:'邮箱地址不合法',required:true, trigger: 'blur' }
+  ],
+  password: [
+    { required:true, message:'密码不能为空', trigger: 'blur' },
+    { min:6,max:30,message:'密码长度在6-30位之间',trigger:'blur'}
+  ]
+})
+// @ts-ignore 忽略系统提供的方法返回类型
+export const {ctx}=getCurrentInstance()
--- demo-management/src/views/Login.vue
+++ demo-management/src/views/Login.vue
@@ -1,41 +1,77 @@
 <template>
-    <div class="container">
+    <div  :class="['container',{ 'sign-up-mode': signUpMode }]">
         <!-- 表单容器 -->
         <div class="form-container">
             <div class="sign">
                 <!-- 登录 -->
-                <div>登录</div>
+                <el-form ref="loginForm" :model="loginUser" :rules="rules" label-width="100px" class="loginForm sign-in-form">
+                  <el-form-item label="邮箱" prop="email">
+                    <el-input v-model="loginUser.email"  placeholder="请输入邮箱"></el-input>
+                  </el-form-item>
+                  <el-form-item label="密码" prop="password">
+                    <el-input v-model="loginUser.password" type="password" placeholder="请输入密码"></el-input>
+                  </el-form-item>
+                  <el-form-item>
+                    <el-button @click="handleLogin('loginForm')" type="primary" class="submit-btn">提交</el-button>
+                  </el-form-item>
+                  <!-- 找回密码 -->
+                  <div class="tiparea">
+                    <p>忘记密码?
+                      <a>立即找回</a>
+                    </p>
+                  </div>
+                </el-form>
                 <!-- 注册 -->
-                <div>注册</div>
+                <!-- <div>注册</div> -->
             </div>  
         </div>
         <div>
           <!-- 左右切换动画 -->
           <div class="panels-container">
-            <div class="panel panel-left">
-              <div class="content">
-                <h3></h3>
-              </div>
-            </div>
-            <div class="panel panel-right">
+            <div class="panel left-panel">
               <div class="content">
                 <h3>业精于勤荒于嬉,行成于思毁于随</h3>
                 <p>何以解忧，唯有杜康</p>
-                <button class="btn transoarent">注册</button>
+                <button @click="signUpMode=!signUpMode" class="btn transparent">注册</button>
               </div>
-              <!-- <img src="@/assets/log.svg" class="image" alt=""> -->
-
+              <img src="@/assets/img/log.svg" class="image" alt="">
+            </div>
+            <div class="panel right-panel">
+              <div class="content">
+                <h3>一寸光阴一寸金,寸金难买寸光阴</h3>
+                <p>慨当以慷，忧思难忘</p>
+                <button @click="signUpMode=!signUpMode" class="btn transparent">登录</button>
+              </div>
+              <img src="@/assets/img/register.svg" class="image" alt="">
             </div>
           </div>
         </div>
     </div>
 </template>
 <script lang="ts">
-import {defineComponent} from 'vue'
+import {defineComponent,ref,getCurrentInstance} from 'vue'
+import {loginUser,rules,ctx} from '@/utils/loginValidate'
 export default defineComponent({
     name:'Login',
-    components:{
-
+    setup(){
+      const signUpMode=ref<boolean>(false)
+      // 登录方法
+      const handleLogin=(formName: string)=>{
+        ctx.$refs[formName].validate((valid: boolean)=>{
+          if(valid){
+            console.log(valid)
+          }else{
+            console.log(valid)
+            return false
+          }
+        })
+      }
+      return{
+        signUpMode,
+        rules,
+        loginUser,
+        handleLogin
+      }
     }
 })
 </script>
@@ -65,4 +101,326 @@ export default defineComponent({
   grid-template-columns: 1fr;
   z-index: 5;
 }
+/**
+左右切换动画样式
+ */
+.social-text {
+  padding: 0.7rem 0;
+  font-size: 1rem;
+}
+
+.social-media {
+  display: flex;
+  justify-content: center;
+}
+
+.social-icon {
+  height: 46px;
+  width: 46px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 0.45rem;
+  color: #333;
+  border-radius: 50%;
+  border: 1px solid #333;
+  text-decoration: none;
+  font-size: 1.1rem;
+  transition: 0.3s;
+}
+
+.social-icon:hover {
+  color: #4481eb;
+  border-color: #4481eb;
+}
+
+.btn {
+  width: 150px;
+  background-color: #5995fd;
+  border: none;
+  outline: none;
+  height: 49px;
+  border-radius: 49px;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: 600;
+  margin: 10px 0;
+  cursor: pointer;
+  transition: 0.5s;
+}
+
+.btn:hover {
+  background-color: #4d84e2;
+}
+.panels-container {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  top: 0;
+  left: 0;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+}
+
+.container:before {
+  content: "";
+  position: absolute;
+  height: 2000px;
+  width: 2000px;
+  top: -10%;
+  right: 48%;
+  transform: translateY(-50%);
+  background-image: linear-gradient(-45deg, #4481eb 0%, #04befe 100%);
+  transition: 1.8s ease-in-out;
+  border-radius: 50%;
+  z-index: 6;
+}
+
+.image {
+  width: 100%;
+  transition: transform 1.1s ease-in-out;
+  transition-delay: 0.4s;
+}
+
+.panel {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: space-around;
+  text-align: center;
+  z-index: 6;
+}
+
+.left-panel {
+  pointer-events: all;
+  padding: 3rem 17% 2rem 12%;
+}
+
+.right-panel {
+  pointer-events: none;
+  padding: 3rem 12% 2rem 17%;
+}
+
+.panel .content {
+  color: #fff;
+  transition: transform 0.9s ease-in-out;
+  transition-delay: 0.6s;
+}
+
+.panel h3 {
+  font-weight: 600;
+  line-height: 1;
+  font-size: 1.5rem;
+}
+
+.panel p {
+  font-size: 0.95rem;
+  padding: 0.7rem 0;
+}
+
+.btn.transparent {
+  margin: 0;
+  background: none;
+  border: 2px solid #fff;
+  width: 130px;
+  height: 41px;
+  font-weight: 600;
+  font-size: 0.8rem;
+}
+
+.right-panel .image,
+.right-panel .content {
+  transform: translateX(800px);
+}
+/**
+*动画样式
+ */
+.container.sign-up-mode:before {
+  transform: translate(100%, -50%);
+  right: 52%;
+}
+
+.container.sign-up-mode .left-panel .image,
+.container.sign-up-mode .left-panel .content {
+  transform: translateX(-800px);
+}
+
+.container.sign-up-mode .signin {
+  left: 25%;
+}
+
+.container.sign-up-mode form.sign-up-form {
+  opacity: 1;
+  z-index: 2;
+}
+
+.container.sign-up-mode form.sign-in-form {
+  opacity: 0;
+  z-index: 1;
+}
+
+.container.sign-up-mode .right-panel .image,
+.container.sign-up-mode .right-panel .content {
+  transform: translateX(0%);
+}
+
+.container.sign-up-mode .left-panel {
+  pointer-events: none;
+}
+
+.container.sign-up-mode .right-panel {
+  pointer-events: all;
+}
+
+@media (max-width: 870px) {
+  .container {
+    min-height: 800px;
+    height: 100vh;
+  }
+  .sign {
+    width: 100%;
+    top: 95%;
+    transform: translate(-50%, -100%);
+    transition: 1s 0.8s ease-in-out;
+  }
+
+  .signin,
+  .container.sign-up-mode .sign {
+    left: 50%;
+  }
+
+  .panels-container {
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr 2fr 1fr;
+  }
+
+  .panel {
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    padding: 2.5rem 8%;
+    grid-column: 1 / 2;
+  }
+
+  .right-panel {
+    grid-row: 3 / 4;
+  }
+
+  .left-panel {
+    grid-row: 1 / 2;
+  }
+
+  .image {
+    width: 200px;
+    transition: transform 0.9s ease-in-out;
+    transition-delay: 0.6s;
+  }
+
+  .panel .content {
+    padding-right: 15%;
+    transition: transform 0.9s ease-in-out;
+    transition-delay: 0.8s;
+  }
+
+  .panel h3 {
+    font-size: 1.2rem;
+  }
+
+  .panel p {
+    font-size: 0.7rem;
+    padding: 0.5rem 0;
+  }
+
+  .btn.transparent {
+    width: 110px;
+    height: 35px;
+    font-size: 0.7rem;
+  }
+
+  .container:before {
+    width: 1500px;
+    height: 1500px;
+    transform: translateX(-50%);
+    left: 30%;
+    bottom: 68%;
+    right: initial;
+    top: initial;
+    transition: 2s ease-in-out;
+  }
+
+  .container.sign-up-mode:before {
+    transform: translate(-50%, 100%);
+    bottom: 32%;
+    right: initial;
+  }
+
+  .container.sign-up-mode .left-panel .image,
+  .container.sign-up-mode .left-panel .content {
+    transform: translateY(-300px);
+  }
+
+  .container.sign-up-mode .right-panel .image,
+  .container.sign-up-mode .right-panel .content {
+    transform: translateY(0px);
+  }
+
+  .right-panel .image,
+  .right-panel .content {
+    transform: translateY(300px);
+  }
+
+  .container.sign-up-mode .signin-signup {
+    top: 5%;
+    transform: translate(-50%, 0);
+  }
+}
+
+@media (max-width: 570px) {
+  form {
+    padding: 0 1.5rem;
+  }
+
+  .image {
+    display: none;
+  }
+  .panel .content {
+    padding: 0.5rem 1rem;
+  }
+  .container {
+    padding: 1.5rem;
+  }
+
+  .container:before {
+    bottom: 72%;
+    left: 50%;
+  }
+
+  .container.sign-up-mode:before {
+    bottom: 28%;
+    left: 50%;
+  }
+}
+/**
+form样式
+ */
+.loginForm {
+  margin-top: 20px;
+  background-color: #fff;
+  padding: 20px 40px 20px 20px;
+  border-radius: 5px;
+  box-shadow: 0px 5px 10px #cccc;
+}
+
+.submit-btn {
+  width: 100%;
+}
+.tiparea {
+  text-align: right;
+  font-size: 12px;
+  color: #333;
+}
+.tiparea p a {
+  color: #409eff;
+}
+
 </style>
