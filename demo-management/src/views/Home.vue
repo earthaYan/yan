397dbf7ba0bdf3e0c7c4c84182cbe34524cd392d
wbyanyue <wbyanyue@centaline.com.cn>
2021-01-06 17:32:04 +0800

    增加跳转登录页按钮

--- demo-management/src/views/Home.vue
+++ demo-management/src/views/Home.vue
@@ -1,12 +1,24 @@
 <template>
     <div>首页</div>
+    <button @click="toLogin">登录页</button>
 </template>
 <script lang="ts">
-import {defineComponent} from 'vue'
+import {defineComponent, getCurrentInstance} from 'vue'
+import { useRouter } from 'vue-router'
 export default defineComponent({
     name:'Home',
     components:{
-
+    },
+    setup(){
+        // @ts-ignore
+        const {ctx}=getCurrentInstance()
+        const router = useRouter()
+        const toLogin=()=>{
+            router.push('/login')
+        }
+        return {
+            toLogin
+        }
     }
 })
 </script>
